9e285117dbd954791b607b45aff1059b16c6154d
hua1314520 <728502583@qq.com>
2019-03-28 09:22:49 +0800

    第一次提交

---
+++ js/百度换肤.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+	var imgs=document.getElementsByTagName("img");
+imgs[0].onclick=function(){
+	document.body.style.backgroundImage="url(img/3.jpg)";
+}
+imgs[1].onclick=function(){
+	document.body.style.backgroundImage="url(img/2.jpg)";
+}
+imgs[2].onclick=function(){
+	document.body.style.backgroundImage="url(img/4.jpg)";
+}
+imgs[3].onclick=function(){
+	document.body.style.backgroundImage="url(img/15.jpg)";
+}
+}
